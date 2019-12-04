faa36f9b32b9d0c7dd3340675b769891fe38c155
RN <ryouya.nakai0103@gmail.com>
2019-12-05 08:56:34 +0900

    変数名の変更

--- KADAI01/ConsoleApp1/Program.cs
+++ KADAI01/ConsoleApp1/Program.cs
@@ -15,18 +15,18 @@ namespace ConsoleApp1
         {
 
             Console.WriteLine("5x^2+3x+1");//問題
-            int X = int.Parse(Console.ReadLine());//intに変換して入力
-            Console.WriteLine(TEST(X));//関数呼び出しと表示
+            int x = int.Parse(Console.ReadLine());//intに変換して入力
+            Console.WriteLine(Test(x));//関数呼び出しと表示
 
         }
-        static int TEST(int XX)
+        static int Test(int x)
         {
-            int AN;//答え保存
-            var NN = Math.Pow(XX, 2);
+            int ans;//答え保存
+            var NN = Math.Pow(x, 2);
             var N = Convert.ToInt32(NN);
-            AN = 5 * (N) + (3 * XX) + 1;//計算式
+            ans = 5 * (N) + (3 * x) + 1;//計算式
 
-            return AN;
+            return ans;
         }
     }
 }
